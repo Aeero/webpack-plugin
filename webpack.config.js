d5037fd237e9a0b502572590a64d2cb427f0cbc8
Aeero <lhf_personal@126.com>
2019-04-28 21:35:17 +0800

    插件的基本写法

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 var path = require('path');
 
+var DemoPlugin = require('./src/plugins/demo');
+
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, './dist')
-  }
+  },
+  plugins: [
+    new DemoPlugin()
+  ]
 }
